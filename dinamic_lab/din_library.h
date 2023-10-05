82ef123ec39cc161a7b5b732637544f9591ddf9d
Igor <jaknikig@mail.ru>
2023-10-05 23:17:45 +0200

    change CMake

--- dinamic_lab/din_library.h
+++ dinamic_lab/din_library.h
@@ -1,11 +1,11 @@
 
 #pragma once
-//#ifndef DINAMIC_LAB_LIBRARY_H_EXPORT
-//#define DINAMIC_LAB_LIBRARY_H __declspec(dllexport)
-//#else
-//#define DINAMIC_LAB_LIBRARY_H __declspec(dllimport)
-//#endif
-set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)
+#ifdef library_din_house_EXPORTS
+#define DINAMIC_LAB_LIBRARY_H __declspec(dllexport)
+#else
+#define DINAMIC_LAB_LIBRARY_H __declspec(dllimport)
+#endif
+//set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)
 #include "string"
 #include "iostream"
 
@@ -13,4 +13,7 @@ set(CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS ON)
     public:
         std::string leave(std::string);
     };
+
+
+
 
